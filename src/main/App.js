f3ca58cc3cc9629f2637d47cfeb6de6950d672ac
nathallyet <nathallyet@gmail.com>
2022-07-29 15:19:25 -0300

    Persistindo dados

--- src/main/App.js
+++ src/main/App.js
@@ -4,6 +4,7 @@ import {
   Routes, 
   Route
 } from "react-router-dom";
+import SimpleStorage from "react-simple-storage";
 
 import Menu from "../components/Menu";
 import TabelaLivros from "../components/TabelaLivros";
@@ -15,24 +16,26 @@ class App extends Component {
 
   state = {
     livros: [
-      {
-        id: 1,
-        isbn: "978-85-7522-403-8",
-        titulo: "HTML5 - 2ª Edição",
-        autor: "Maurício Samy Silva",
-      },
-      {
-        id: 2,
-        isbn: "978-85-7522-807-4",
-        titulo: "Introdução ao Pentest",
-        autor: "Daniel Moreno",
-      },
-      {
-        id: 3,
-        isbn: "978-85-7522-780-8",
-        titulo: "Internet das Coisas para Desenvolvedores",
-        autor: "Ricardo da Silva Ogliari",
-      }
+      // itens removidos do objeto inicial state para persistência dos dados
+      // deixando o objeto state recebendo a chave livros cujo o valor é um array vazio []
+      // {
+      //   id: 1,
+      //   isbn: "978-85-7522-403-8",
+      //   titulo: "HTML5 - 2ª Edição",
+      //   autor: "Maurício Samy Silva",
+      // },
+      // {
+      //   id: 2,
+      //   isbn: "978-85-7522-807-4",
+      //   titulo: "Introdução ao Pentest",
+      //   autor: "Daniel Moreno",
+      // },
+      // {
+      //   id: 3,
+      //   isbn: "978-85-7522-780-8",
+      //   titulo: "Internet das Coisas para Desenvolvedores",
+      //   autor: "Ricardo da Silva Ogliari",
+      // }
     ]
   }
 
@@ -64,6 +67,7 @@ class App extends Component {
   render() {
     return (
       <Router >
+        <SimpleStorage parent={this} />
         <Menu />
         <Routes>
           <Route exact path="/" 
